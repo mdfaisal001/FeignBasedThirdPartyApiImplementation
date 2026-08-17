032d16bda9cbc5b86cfcb9eae558f743ce52e36c
Md Faisal <mdfaisalsyed2310@gmail.com>
2026-08-17 12:46:21 +0530

    Seaparated the query parameter apikey to the interceptors successfully

--- feign/src/main/java/com/practise/feign/client/WeatherClient.java
+++ feign/src/main/java/com/practise/feign/client/WeatherClient.java
@@ -1,5 +1,6 @@
 package com.practise.feign.client;
 
+import com.practise.feign.config.OpenWeatherFeignConfig;
 import com.practise.feign.dto.OpenWeatherResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "openweather",
-        url = "${openweather.base-url}"
+        url = "${openweather.base-url}",
+        configuration = OpenWeatherFeignConfig.class
 )
 public interface WeatherClient {
 
     @GetMapping("/data/2.5/weather")
     OpenWeatherResponse getWeather(
             @RequestParam("q") String city,
-            @RequestParam("units") String units,
-            @RequestParam("appid") String apiKey
+            @RequestParam("units") String units
+
     );
 }
--- feign/src/main/java/com/practise/feign/service/WeatherService.java
+++ feign/src/main/java/com/practise/feign/service/WeatherService.java
@@ -4,17 +4,15 @@ import com.practise.feign.client.WeatherClient;
 import com.practise.feign.dto.OpenWeatherResponse;
 import com.practise.feign.dto.WeatherResponse;
 import com.practise.feign.mapper.WeatherMapper;
-import jakarta.websocket.server.ServerEndpoint;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
+
 
 @Service
 public class WeatherService {
     private final WeatherClient weatherClient;
     private final WeatherMapper weatherMapper;
-    @Value("${openweather.api-key}")
-    String apiKey;
+
     public WeatherService(WeatherClient weatherClient, WeatherMapper weatherMapper){
         this.weatherClient = weatherClient;
         this.weatherMapper = weatherMapper;
@@ -23,10 +21,8 @@ public class WeatherService {
     public WeatherResponse getWeather(String city){
         OpenWeatherResponse response =weatherClient.getWeather(
                 city,
-                "metric",
-                apiKey
+                "metric"
         );
-
         return weatherMapper.toResponse(response);
     }
 }
