6078d264f2000d7069604a93194964fd25bf4fec
Md Faisal <mdfaisalsyed2310@gmail.com>
2026-08-28 18:11:23 +0530

    Revert "Successfully unit tested the WeatherMapper with 3 cases"
    This reverts commit ec5ed5d11b39abbbafa15aa60c2faa3ec2a8d764.

--- feign/src/main/java/com/practise/feign/mapper/WeatherMapper.java
+++ feign/src/main/java/com/practise/feign/mapper/WeatherMapper.java
@@ -13,7 +13,6 @@ public interface WeatherMapper {
     @Mapping(target = "temperature", source = "main.temp")
     @Mapping(target = "feelsLike", source = "main.feelsLike")
     @Mapping(target = "description", source = "weather")
-    @Mapping(target = "humidity", source = "main.humidity")
     WeatherResponse toResponse(OpenWeatherResponse response);
 
     default String mapDescription(
